b5f2ecc4649ce8609c3d7a81a71f407972398eea
Santiago Cabrera <64879702+Santiago-cabrera-Arias@users.noreply.github.com>
2020-11-10 12:34:14 -0500

    Creacion de modelo

---
+++ src/ups/edu/ec/modelo/Telefono.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ups.edu.ec.modelo;
+
+/**
+ *
+ * @author santi
+ */
+public class Telefono {
+
+    private int id;
+    private String numero;
+    private String operadora;
+    private EnumTipo tipo;
+
+    public Telefono(int id, String numero, String operadora, EnumTipo tipo) {
+        this.id = id;
+        this.numero = numero;
+        this.operadora = operadora;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getOperadora() {
+        return operadora;
+    }
+
+    public void setOperadora(String operadora) {
+        this.operadora = operadora;
+    }
+
+    public EnumTipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(EnumTipo tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Telefono other = (Telefono) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefono{" + "id=" + id + ", numero=" + numero + ", operadora=" + operadora + ", tipo=" + tipo + '}';
+    }
+
+}
