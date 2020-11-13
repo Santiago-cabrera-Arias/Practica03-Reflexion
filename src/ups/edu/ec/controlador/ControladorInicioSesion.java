b04740b9a594eda1cd3d4ce60e2575ec92510581
Santiago Cabrera <64879702+Santiago-cabrera-Arias@users.noreply.github.com>
2020-11-13 02:23:31 -0500

    Creacion del controlador inciar Sesion para la utilizacion de Singleton

---
+++ src/ups/edu/ec/controlador/ControladorInicioSesion.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ups.edu.ec.controlador;
+
+import java.util.ArrayList;
+import java.util.List;
+import ups.edu.ec.modelo.datosDeUsuario;
+
+/**
+ *
+ * @author santi
+ */
+public class ControladorInicioSesion {
+    
+    private static ControladorInicioSesion instancia = new ControladorInicioSesion();
+    
+    private List lista = new ArrayList();
+    
+    private ControladorInicioSesion(){
+        
+    }
+    
+    public static ControladorInicioSesion getIntancia(){
+        
+        return instancia;
+        
+    }
+    
+    public boolean registrar(datosDeUsuario objeto){
+        
+        if (objeto != null) 
+            return lista.add(objeto);
+            
+            return false;
+        
+    }
+    
+    public datosDeUsuario validacion(datosDeUsuario objeto){
+        
+        if (lista.size() > 0 ) {
+            
+            for (Object object : lista) {
+                
+                if (object.equals(objeto)) {
+                    
+                    return objeto;
+                    
+                }
+                
+            }
+            
+        }
+        
+        return null;
+        
+    }
+    
+}
